cffc0671600596424a8fdbf54fd2155657425be9
Adam Daw <adam@adamdaw.com>
2014-03-30 18:12:43 -0400

    Add cask for LexiLoader v29
    This commit adds a cask for LexiLoader, an ApexDataLoader by Simon Fell
    (@superfell) for OSX. Essential tool for Force.com Data work on OS X!

---
+++ Casks/lexi-loader.rb
@@ -0,0 +1,7 @@
+class LexiLoader < Cask
+  url 'https://dl.dropboxusercontent.com/u/6682148/LexiLoader_v29_2.zip'
+  homepage 'http://www.pocketsoap.com/osx/lexiloader/'
+  version '29'
+  no_checksum
+  link 'LexiLoader_v29.app'
+end
