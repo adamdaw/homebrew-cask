254b2e25e2967143164e2e272e33b7cc7ed36c56
Adam Daw <adam@adamdaw.com>
2014-03-30 18:14:57 -0400

    Add cask for Trapdoor.app (v1.8)
    This commit adds a cask for Trapdoor, a multi-login manager for
    Force.com orgs created by Simon Fell (@superfell).

---
+++ Casks/trapdoor.rb
@@ -0,0 +1,7 @@
+class Trapdoor < Cask
+  url 'http://www.pocketsoap.com/osx/trapdoor/trapdoor_v180.zip'
+  homepage 'http://www.pocketsoap.com/osx/trapdoor/'
+  version '1.8'
+  no_checksum
+  link 'Trapdoor.app'
+end
